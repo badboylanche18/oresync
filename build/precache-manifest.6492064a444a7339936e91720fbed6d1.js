cdb1f91d480a0c5a9154e6fb7c2640c65b6dbc33
oforduhh <oforduhh@gmail.com>
2022-05-10 14:31:18 +0100

    first commit

---
+++ build/precache-manifest.6492064a444a7339936e91720fbed6d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b638641af1bc7357f52a053cd0f49c19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1dadb816ac5c0b5d07d",
+    "url": "/static/css/2.b2de90d5.chunk.css"
+  },
+  {
+    "revision": "5d148562eefcf9576052",
+    "url": "/static/css/main.9cb0f5bc.chunk.css"
+  },
+  {
+    "revision": "c1dadb816ac5c0b5d07d",
+    "url": "/static/js/2.3127648e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3127648e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d148562eefcf9576052",
+    "url": "/static/js/main.6313bd21.chunk.js"
+  },
+  {
+    "revision": "c3655119f17e286e58a8",
+    "url": "/static/js/runtime-main.dbfcc0d1.js"
+  },
+  {
+    "revision": "3d36cd7b0d566cc89251de41dcef180f",
+    "url": "/static/media/error.3d36cd7b.png"
+  }
+]);
